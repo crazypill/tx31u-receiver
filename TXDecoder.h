c252affcb23069a3205bf120c59491abd874812e
Alex Lelievre <alex@folabs.com>
2020-07-17 11:08:42 -0700

    Changes to break out the frame struct

--- TXDecoder.h
+++ TXDecoder.h
@@ -3,34 +3,8 @@
 
 #include "Arduino.h"
 
+#include "TxDecoderFrame.h"
 
-
-enum
-{
-    kDataFlag_temp     = 1 << 0,
-    kDataFlag_humidity = 1 << 1,
-    kDataFlag_rain     = 1 << 2,
-    kDataFlag_wind     = 1 << 3,
-    kDataFlag_gust     = 1 << 4
-};
-
-
-typedef struct
-{
-    uint8_t   magic;
-    uint8_t   station_id;
-    uint8_t   flags;         // the flags tell you which fields are valid in this message
-    uint8_t   humidity;      // 0 - 100
-    float     tempC;         // celsius
-    float     windSpeedMs;   // meters/sec
-    float     windGustMs;    // meters/sec
-    float     rain;
-    float     windDirection; // in degrees
-    uint8_t   CRC;           // will be zero if did not match
-    uint8_t   frameLength;
-} __attribute__ ((__packed__)) Frame;
-
-const uint8_t FRAME_LENGTH = 13;
 
 uint8_t CalculateCRC( uint8_t data[], uint8_t frameLength = FRAME_LENGTH );
 uint8_t DecodeFrame( uint8_t* data, Frame* frame);
---
+++ TXDecoderFrame.h
@@ -0,0 +1,39 @@
+//
+//  TXDecoderFrame.h
+//  
+//
+//  Created by Alex Lelievre on 7/17/20.
+//
+
+#include <stdint.h>
+
+enum
+{
+    kDataFlag_temp     = 1 << 0,
+    kDataFlag_humidity = 1 << 1,
+    kDataFlag_rain     = 1 << 2,
+    kDataFlag_wind     = 1 << 3,
+    kDataFlag_gust     = 1 << 4,
+    kDataFlag_intTemp  = 1 << 5,
+    kDataFlag_pressure = 1 << 6
+};
+
+
+typedef struct
+{
+    uint8_t   magic;
+    uint8_t   station_id;
+    uint8_t   flags;         // the flags tell you which fields are valid in this message
+    uint8_t   humidity;      // 0 - 100
+    float     tempC;         // temp from weather sensor in celsius
+    float     intTempC;      // internal temp in celsius
+    float     pressureInHg;  // internal pressure in inches mercury
+    float     windSpeedMs;   // meters/sec
+    float     windGustMs;    // meters/sec
+    float     rain;
+    float     windDirection; // in degrees
+    uint8_t   CRC;           // just used as part of validation, really doesn't need to be here at all
+    uint8_t   frameLength;   // ditto with this guy
+} __attribute__ ((__packed__)) Frame;
+
+const uint8_t FRAME_LENGTH = 13;
